5941499406d5260be7ffdb28740579e4124fea12
Sebastian Larsson <sebastian.larsson@cybercom.com>
2015-01-27 19:10:41 +0100

    failed the build...

--- src/test/java/controller/ControllerTest.java
+++ src/test/java/controller/ControllerTest.java
@@ -1,10 +1,13 @@
 package controller;
 
+import static org.junit.Assert.fail;
+
 import org.junit.Test;
 
 public class ControllerTest {
 	@Test
 	public void runGame() {
 		Controller.main();
+		fail();
 	}
 }
