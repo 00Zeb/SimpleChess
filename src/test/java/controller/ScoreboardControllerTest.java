7961aeb26a9caa70c62d6530b49e15c7f9e8909c
Sebastian Larsson <sebastian.larsson@cybercom.com>
2015-02-21 15:02:23 +0100

    added test

--- src/test/java/controller/ScoreboardControllerTest.java
+++ src/test/java/controller/ScoreboardControllerTest.java
@@ -2,7 +2,7 @@ package controller;
 
 import java.util.List;
 
-import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,21 +10,39 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import web.ScoreboardController;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
 public class ScoreboardControllerTest {
 	@Configuration
-	@ComponentScan({"player", "controller"})
+	@ComponentScan({ "player", "controller", "web" })
 	public static class Config {
-		
+
 	}
-	
+
 	@Autowired
 	private List<Player> players;
-	
-	@Test
-	public void testName() throws Exception {
-		Assert.assertTrue(players.size() >= 2);
+	@Autowired
+	private ScoreboardController scoreboardController;
+	private MockMvc mockMvc;
+
+	@Before
+	public void create() throws Exception {
+		mockMvc = MockMvcBuilders.standaloneSetup(scoreboardController).build();
 	}
+
+	@Test
+	public void performGet() throws Exception {
+		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.get("/");
+		mockMvc.perform(mockRequest).andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.model().attributeExists("results"));
+	}
+
 }
