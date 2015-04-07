58c254ab413c16d890e505bb67d59807d820e694
Per Skansfors <per.skansfors@gmail.com>
2015-04-07 08:32:22 +0200

    Passing score multiplier argument from cmd line

--- src/main/java/web/Application.java
+++ src/main/java/web/Application.java
@@ -7,8 +7,10 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan({"player","controller","web"})
 @EnableAutoConfiguration
 public class Application {
+	public static String[] ARGS = new String[0];
 
     public static void main(String[] args) {
+    	ARGS = args;
         SpringApplication.run(Application.class, args);
     }
 
