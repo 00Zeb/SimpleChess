df260409a052830da6207ab961f33a27f0651d83
Per Skansfors <per.skansfors@gmail.com>
2015-04-12 19:05:39 +0200

    Made CybercomPlayer abstract

--- src/main/java/controller/CybercomPlayer.java
+++ src/main/java/controller/CybercomPlayer.java
@@ -1,12 +1,6 @@
 package controller;
 
 
-public class CybercomPlayer extends Player{
-
-	@Override
-	public Move getMove(Board board, Player enemy) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+public abstract class CybercomPlayer extends Player{
 
 }
